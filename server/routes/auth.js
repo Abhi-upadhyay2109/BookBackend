be6aca44bbdd1ac3f40376a136c08a6ce35c8ad4
Abhi-upadhyay2109 <panditabhishek9651@gmail.com>
2025-07-13 19:55:18 +0530

    http

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -56,7 +56,7 @@ router.post('/login', async (req, res) => {
     const token = generateToken(user._id);
     
     res.cookie('token', token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       maxAge: 86400000 
